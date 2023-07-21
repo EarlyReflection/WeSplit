fe8b6bc580df2ffe93f668b5429af58a2024648f
Vladimir Dvornikov <earlyreflections@protonmail.com>
2023-07-21 21:45:31 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Vladimir Dvornikov on 21/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
