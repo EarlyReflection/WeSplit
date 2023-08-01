ad4d5b1f02dc4fe57a71bdfaecf57d9befe89aab
Vladimir Dvornikov <earlyreflections@protonmail.com>
2023-08-01 22:24:03 +0300

    change text color from .black to .primary

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -60,7 +60,7 @@ struct ContentView: View {
                 
                 Section {
                     Text (totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                        .foregroundColor(tipPercentage == 0 ? .red : .black)
+                        .foregroundColor(tipPercentage == 0 ? .red : .primary)
                 } header: {
                     Text("Total amount")
                 }
